d9813ff64b11fd35d6f0737013d591bf39f873eb
claudio daniele <c.daniele@gmx.de>
2020-05-26 17:07:17 +0200

    all bookings and delete function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,10 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def delete
+    @booking.destroy
+    redirect_to bikes_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :bikes, only: [:new, :create, :index, :show, :edit, :update]
+  resources :bookings, only: [:index, :destroy]
 end
