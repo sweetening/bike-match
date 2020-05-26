1008b2c7f4bdcce945a3af765ebd6b73e7b011ef
Victoria Gonzales <vagp89@gmail.com>
2020-05-26 14:48:05 +0200

    edit and update route and contoller bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,22 @@
 class BikesController < ApplicationController
+
+  def edit
+  end
+
+  def update
+    if @bikes.update(params[:id])
+      redirect_to edit_path(@bikes)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+   # strong params
+  def bikes_params
+    params.require(:bike).permit(:type, :location, :price, :title, :image_url,
+    :user_id, :lat, :long)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/bikes/:id/edit", to: "bikes#edit", as: :edit_bike
+  patch "/bikes/:id", to: "bikes#update"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
