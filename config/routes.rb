a8c53edd6abdb1edb7041f36702bd97c76d17288
Victoria Gonzales <62341916+vagp89@users.noreply.github.com>
2020-05-26 18:26:41 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :bikes, only: [:new, :create, :index, :show, :edit, :update] do
-   resources :bookings, only: [:index, :new, :create, :edit, :update, :show]
+   resources :bookings, only: [:new, :create, :edit, :update, :show]
  end
   resources :bookings, only: [:delete]
 end
