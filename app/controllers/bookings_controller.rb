a8fe1dc9cdb329b8dd3e7ac48bd06771f7f0fd7f
Victoria Gonzales <vagp89@gmail.com>
2020-05-26 17:04:00 +0200

    edit booking and view page show and the destroy route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,6 @@
 class BookingsController < ApplicationController
+
+def show
+ @booking = bike.find(params[:id])
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :bikes, only: [:new, :create, :index, :show, :edit, :update]
+  resources :bikes, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :bookings, only: [:show]
+  end
 end
