509b2ceff88bb361a2fda7da7c738b2028641223
Jocelyn Proctor <jocelynkproctor@gmail.com>
2020-05-26 14:10:06 +0200

    created bikes show page and routes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,15 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: [:show]
+
+  def show; end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:type, :location, :price, :title, :image_url, :lat, :long)
+  end
 end
