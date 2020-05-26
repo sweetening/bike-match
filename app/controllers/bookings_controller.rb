6db1420f14fb8f5ec66e0f493157e6c966ebbfd6
Sanja Wetzel <sanjwich@gmail.com>
2020-05-26 16:47:48 +0200

    Edit booking form completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_bike, only: [:edit, :update]
+  def edit;end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bikes_path(@bike)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+   def booking_params
+    params.require(:booking).permit(:total_price, :start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :bikes, only: [:new, :create, :index, :show, :edit, :update]
+  resources :bikes, only: [:new, :create, :index, :show, :edit, :update] do
+   resources :bookings, only: [:edit, :update]
+ end
 end
