3bbfaa418ae47336a200e611be71d0eb11769ba5
Jocelyn Proctor <jocelynkproctor@gmail.com>
2020-05-28 23:18:39 +0200

    added reviews controller and views

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings
+  has_many :reviews, through: :bookings, dependent: :destroy
   has_many_attached :photos
   validates :title, :price, :photos, presence: true
   validates :title, uniqueness: true
